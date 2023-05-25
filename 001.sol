ab3b44898dd7cfdd580557e137f4c87851844f55
levy <levii@naver.com>
2023-05-25 18:47:09 +0900

    MONKEYINU.sol

---
+++ 001.sol
@@ -0,0 +1,595 @@
+/**
+ *Submitted for verification at BscScan.com on 2023-04-30
+*/
+
+// SPDX-License-Identifier: UNLICENSE
+// pragma solidity 0.8.17;
+
+// overlap
+interface IBEP20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+// overlap
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+// meaning that 'Ownable' inherits the 'Context'
+// Abstract contracts cannot be instantiated and must be inherited from other contracts
+// Typically used to define abstract members, such as fuctions or events,
+// that should be implemented in a derived contract, without proving an implemetation
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// Used on platforms that allow DEX token trading
+interface IFactory {
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+}
+
+interface IRouter {
+    function factory() external pure returns (address);
+
+    function WETH() external pure returns (address);
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+}
+
+// securely transfer Ether to a given address
+// keeping the code of the contract concise
+library Address {
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        (bool success, ) = recipient.call{ value: amount }("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+}
+
+
+contract MONKEYINU is Context, IBEP20, Ownable {
+
+    // use the "Address" library to implement address-related function
+    using Address for address payable;
+
+
+    // 각 주소의 현재 잔액을 추적합니다.
+    mapping(address => uint256) private _rOwned;
+
+    // 수수료가 적용되지 않은 주소들의 잔액을 추적합니다.
+    mapping(address => uint256) private _tOwned;
+
+    // 허용된 인출 금액을 추적합니다
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    // 수수료가 적용되지 않는 주소를 추적합니다.
+    mapping(address => bool) private _isExcludedFromFee;
+
+    // 보상이 제외된 주소를 추적합니다.
+    mapping(address => bool) private _isExcluded;
+
+    address[] private _excluded;
+
+    // 현재 스왑이 진행 중인지를 나타내는 변수
+    bool private swapping;
+
+    // 이 둘은 PancakeSwap (BSC에서) 또는 Uniswap (ETH에서)의 라우터와 이 토큰과 연결된 페어를 가리킵니다
+    IRouter public router;
+    address public pair;
+    
+    // 토큰의 소수점 이하 자릿수, 총 공급량, 그리고 '_r' 스케일에서의 총 공급량을 설정합니다.
+    uint8 private constant _decimals = 9;
+    uint256 private constant MAX = ~uint256(0);
+    uint256 private _tTotal = 100*10**8 * 10**_decimals;
+    uint256 private _rTotal = (MAX - (MAX % _tTotal));
+
+
+    // 자동 리퀴디티 추가 기능이 작동하는 토큰 임계값을 설정
+    uint256 public swapTokensAtAmount = _tTotal/5000;
+
+    event SwapTokensAtAmountUpdated(uint256 indexed previousValue, uint256 indexed newValue);
+
+    // 각각 토큰이 소각될 주소와 마케팅 관련 자금이 전송될 주소를 가리킵니다.
+    address public deadWallet = 0x000000000000000000000000000000000000dEaD;
+    address public marketingWallet = 0x7156ED3a96d2f1B7044f5eEaD0335bC6E1471A34;
+
+    string private constant _name = "Monkey Inu";
+    string private constant _symbol = "MONKEYINU";
+
+    // 거래시 수집되는 수수료와 지금까지 수집된 총 수수료를 관리합니다. 
+    // 여기서는 5%의 리워드와 5%의 마케팅 수수료가 설정되어 있습니다
+    struct Taxes {
+        uint256 rfi;
+        uint256 marketing;
+    }
+    // tax 5% reward, 5% marketing
+    Taxes public taxes = Taxes(5, 5);
+
+    struct TotFeesPaidStruct {
+        uint256 rfi;
+        uint256 marketing;
+    }
+
+    TotFeesPaidStruct public totFeesPaid;
+
+    struct valuesFromGetValues {
+        uint256 rAmount;
+        uint256 rTransferAmount;
+        uint256 rRfi;
+        uint256 rMarketing;
+        uint256 tTransferAmount;
+        uint256 tRfi;
+        uint256 tMarketing;
+    }
+
+    // 중복된 스왑을 방지하기 위한 modifier입니다
+    modifier lockTheSwap() {
+        swapping = true;
+        _;
+        swapping = false;
+    }
+
+    // 라우터 주소를 설정하고, 라우터와 페어를 생성하고, 보상 제외 주소를 설정하며,
+    // 최초의 _rOwned 잔액을 설정하고, 수수료 제외 주소를 설정합니다.
+    constructor() {
+
+        address routerAddress;
+        if (block.chainid == 56) {
+            routerAddress = 0x10ED43C718714eb63d5aA57B78B54704E256024E; // BSC Pancake Mainnet Router
+        } else if (block.chainid == 97) {
+            routerAddress = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1; // BSC Pancake Testnet Router
+        } else if (block.chainid == 1 || block.chainid == 5) {
+            routerAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D; // ETH Uniswap Mainnet % Testnet
+        } else {
+            revert();
+        }
+
+        IRouter _router = IRouter(routerAddress);
+        address _pair = IFactory(_router.factory()).createPair(address(this), _router.WETH());
+
+        router = _router;
+        pair = _pair;
+
+        excludeFromReward(pair);
+        excludeFromReward(deadWallet);
+
+        _rOwned[owner()] = _rTotal;
+        _isExcludedFromFee[address(this)] = true;
+        _isExcludedFromFee[owner()] = true;
+        _isExcludedFromFee[marketingWallet] = true;
+        _isExcludedFromFee[deadWallet] = true;
+        emit Transfer(address(0), owner(), _tTotal);
+    }
+
+    //std BEP20:
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return _decimals;
+    }
+
+    //override BEP20:
+    function totalSupply() public view override returns (uint256) {
+        return _tTotal;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        if (_isExcluded[account]) return _tOwned[account];
+        return tokenFromReflection(_rOwned[account]);
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "BEP20: transfer amount exceeds allowance");
+        _approve(sender, _msgSender(), currentAllowance - amount);
+
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool){
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "BEP20: decreased allowance below zero");
+        _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+
+        return true;
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function isExcludedFromReward(address account) public view returns (bool) {
+        return _isExcluded[account];
+    }
+
+
+    function reflectionFromToken(uint256 tAmount, bool deductTransferRfi)
+        public
+        view
+        returns (uint256)
+    {
+        require(tAmount <= _tTotal, "Amount must be less than supply");
+        if (!deductTransferRfi) {
+            valuesFromGetValues memory s = _getValues(tAmount, true);
+            return s.rAmount;
+        } else {
+            valuesFromGetValues memory s = _getValues(tAmount, true);
+            return s.rTransferAmount;
+        }
+    }
+
+    function tokenFromReflection(uint256 rAmount) public view returns (uint256) {
+        require(rAmount <= _rTotal, "Amount must be less than total reflections");
+        uint256 currentRate = _getRate();
+        return rAmount / currentRate;
+    }
+
+    //@dev kept original RFI naming -> "reward" as in reflection
+    function excludeFromReward(address account) public onlyOwner {
+        require(!_isExcluded[account], "Account is already excluded");
+        if (_rOwned[account] > 0) {
+            _tOwned[account] = tokenFromReflection(_rOwned[account]);
+        }
+        _isExcluded[account] = true;
+        _excluded.push(account);
+    }
+
+    function includeInReward(address account) external onlyOwner {
+        require(_isExcluded[account], "Account is not excluded");
+
+        for (uint256 i = 0; i < _excluded.length; i++) {
+            if (_excluded[i] == account) {
+                _excluded[i] = _excluded[_excluded.length - 1];
+                _tOwned[account] = 0;
+                _isExcluded[account] = false;
+                _excluded.pop();
+                break;
+            }
+        }
+        
+    }
+
+    function excludeFromFee(address account) public onlyOwner {
+        _isExcludedFromFee[account] = true;
+    }
+
+    function includeInFee(address account) public onlyOwner {
+        _isExcludedFromFee[account] = false;
+    }
+
+    function isExcludedFromFee(address account) public view returns (bool) {
+        return _isExcludedFromFee[account];
+    }
+
+    function _reflectRfi(uint256 rRfi, uint256 tRfi) private {
+        _rTotal -= rRfi;
+        totFeesPaid.rfi += tRfi;
+    }
+
+
+    function _takeMarketing(uint256 rMarketing, uint256 tMarketing) private {
+        totFeesPaid.marketing += tMarketing;
+
+        if (_isExcluded[address(this)]) {
+            _tOwned[address(this)] += tMarketing;
+        }
+        _rOwned[address(this)] += rMarketing;
+    }
+
+
+    function _getValues(
+        uint256 tAmount,
+        bool takeFee
+    ) private view returns (valuesFromGetValues memory to_return) {
+        to_return = _getTValues(tAmount, takeFee);
+        (
+            to_return.rAmount,
+            to_return.rTransferAmount,
+            to_return.rRfi,
+            to_return.rMarketing
+        ) = _getRValues(to_return, tAmount, takeFee, _getRate());
+
+        return to_return;
+    }
+
+    function _getTValues(
+        uint256 tAmount,
+        bool takeFee
+    ) private view returns (valuesFromGetValues memory s) {
+        if (!takeFee) {
+            s.tTransferAmount = tAmount;
+            return s;
+        }
+
+        s.tRfi = (tAmount * taxes.rfi) / 100;
+        s.tMarketing = (tAmount * taxes.marketing) / 100;
+        s.tTransferAmount =
+            tAmount -
+            s.tRfi -
+            s.tMarketing;
+        return s;
+    }
+
+    function _getRValues(
+        valuesFromGetValues memory s,
+        uint256 tAmount,
+        bool takeFee,
+        uint256 currentRate
+    )
+        private
+        pure
+        returns (
+            uint256 rAmount,
+            uint256 rTransferAmount,
+            uint256 rRfi,
+            uint256 rMarketing
+        )
+    {
+        rAmount = tAmount * currentRate;
+
+        if (!takeFee) {
+            return (rAmount, rAmount, 0, 0);
+        }
+
+        rRfi = s.tRfi * currentRate;
+        rMarketing = s.tMarketing * currentRate;
+        rTransferAmount =
+            rAmount -
+            rRfi -
+            rMarketing;
+        return (rAmount, rTransferAmount, rRfi, rMarketing);
+    }
+
+    function _getRate() private view returns (uint256) {
+        (uint256 rSupply, uint256 tSupply) = _getCurrentSupply();
+        return rSupply / tSupply;
+    }
+
+    function _getCurrentSupply() private view returns (uint256, uint256) {
+        uint256 rSupply = _rTotal;
+        uint256 tSupply = _tTotal;
+        for (uint256 i = 0; i < _excluded.length; i++) {
+            if (_rOwned[_excluded[i]] > rSupply || _tOwned[_excluded[i]] > tSupply)
+                return (_rTotal, _tTotal);
+            rSupply = rSupply - _rOwned[_excluded[i]];
+            tSupply = tSupply - _tOwned[_excluded[i]];
+        }
+        if (rSupply < _rTotal / _tTotal) return (_rTotal, _tTotal);
+        return (rSupply, tSupply);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) private {
+        require(owner != address(0), "BEP20: approve from the zero address");
+        require(spender != address(0), "BEP20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) private {
+        require(from != address(0), "BEP20: transfer from the zero address");
+        require(to != address(0), "BEP20: transfer to the zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        require(
+            amount <= balanceOf(from),
+            "You are trying to transfer more than your balance"
+        );
+
+        bool canSwap = balanceOf(address(this)) >= swapTokensAtAmount;
+        if (
+            !swapping &&
+            canSwap &&
+            from != pair &&
+            !_isExcludedFromFee[from] &&
+            !_isExcludedFromFee[to]
+        ) {
+            swapAndLiquify();
+        }
+        bool takeFee = true;
+        if (swapping || _isExcludedFromFee[from] || _isExcludedFromFee[to]) takeFee = false;
+
+        _tokenTransfer(from, to, amount, takeFee);
+    }
+
+    //this method is responsible for taking all fee, if takeFee is true
+    function _tokenTransfer(
+        address sender,
+        address recipient,
+        uint256 tAmount,
+        bool takeFee
+    ) private {
+
+        valuesFromGetValues memory s = _getValues(tAmount, takeFee);
+
+        if (_isExcluded[sender]) {
+            //from excluded
+            _tOwned[sender] = _tOwned[sender] - tAmount;
+        }
+        if (_isExcluded[recipient]) {
+            //to excluded
+            _tOwned[recipient] = _tOwned[recipient] + s.tTransferAmount;
+        }
+
+        _rOwned[sender] = _rOwned[sender] - s.rAmount;
+        _rOwned[recipient] = _rOwned[recipient] + s.rTransferAmount;
+
+        if (s.rRfi > 0 || s.tRfi > 0) _reflectRfi(s.rRfi, s.tRfi);
+        if (s.rMarketing > 0 || s.tMarketing > 0) _takeMarketing(s.rMarketing, s.tMarketing);
+        emit Transfer(sender, recipient, s.tTransferAmount);
+    }
+
+    // 
+    function swapAndLiquify() private lockTheSwap {
+       
+        uint256 contractBalance = balanceOf(address(this));
+        swapTokensForBNB(contractBalance);
+        uint256 deltaBalance = address(this).balance;
+       
+        if (deltaBalance > 0) {
+            payable(marketingWallet).sendValue(deltaBalance);
+        }
+
+    }
+
+    function swapTokensForBNB(uint256 tokenAmount) private {
+        // generate the pair path of token -> weth
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = router.WETH();
+
+        _approve(address(this), address(router), tokenAmount);
+
+        // make the swap
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            tokenAmount,
+            0, // accept any amount of ETH
+            path,
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function bulkExcludeFee(address[] memory accounts, bool state) external onlyOwner {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            _isExcludedFromFee[accounts[i]] = state;
+        }
+    }
+
+    function updateMarketingWallet(address newWallet) external onlyOwner {
+        require(newWallet != address(0),"Fee Address cannot be zero address");
+        marketingWallet = newWallet;
+    }
+
+    function updateSwapTokensAtAmount(uint256 amount) external onlyOwner {
+        require(amount <= 1e15, "Cannot set swap threshold amount higher than 1% of tokens");
+        emit SwapTokensAtAmountUpdated(swapTokensAtAmount,amount * 10**_decimals);
+        swapTokensAtAmount = amount * 10**_decimals;
+    }
+
+    //Use this in case BNB are sent to the contract by mistake
+    function rescueBNB(uint256 weiAmount) external onlyOwner {
+        require(address(this).balance >= weiAmount, "insufficient BNB balance");
+        payable(msg.sender).transfer(weiAmount);
+    }
+
+    //Use this in case BEP20 Tokens are sent to the contract by mistake
+    function rescueAnyBEP20Tokens(address _tokenAddr,address _to, uint256 _amount) public onlyOwner {
+        require(_tokenAddr != address(this), "Owner can't claim contract's balance of its own tokens");
+        IBEP20(_tokenAddr).transfer(_to, _amount);
+    }
+
+    receive() external payable {}
+}
